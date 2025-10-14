419555d304fea8f9e38b2e4b6574327791f16d7d
Sin <xinzechao@SindeMacBook-Air.local>
2025-10-14 18:48:47 +0800

    add sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://bibleverse-generator.org', // 你的主域名
+    generateRobotsTxt: true,                     // 自动生成 robots.txt
+    sitemapSize: 5000,
+    changefreq: 'daily',                         // 建议搜索引擎每天爬取
+    priority: 0.7,
+    exclude: ['/server-sitemap.xml'],            // 如果未来添加自定义 sitemap 可用
+    robotsTxtOptions: {
+      policies: [
+        { userAgent: '*', allow: '/' },
+      ],
+    },
+  };
+  
